5981d23a2a86e114454446993a56a296b39e8ccf
youssef laachari <yousf23@hotmail.fr>
2022-07-17 20:16:39 +0100

    Merge remote-tracking branch 'origin/master'

--- lib/Mixins/test.dart
+++ lib/Mixins/test.dart
@@ -4,6 +4,8 @@ void main() {
   print(yoo.color);
 
   yoo.rotation();
+
+  yoo.miz();
 }
 
 abstract class Shape {
@@ -11,7 +13,7 @@ abstract class Shape {
   Shape({this.color = 'green'});
 }
 
-mixin ShapeRotation {
+class ShapeRotation {
   String direction = 'clockwise';
   double speed = 6.0;
   void rotation() {
@@ -19,8 +21,14 @@ mixin ShapeRotation {
   }
 }
 
-class Circle extends Shape with ShapeRotation {
+class Circle extends Shape with ShapeRotation, test {
   double raduis;
 
   Circle(this.raduis);
+}
+
+class test {
+  void miz() {
+    print('miwwww');
+  }
 }
